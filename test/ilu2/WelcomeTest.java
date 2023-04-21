6226fcab6bbdb3e6e1608957d54f5f58902c1b87
Jarod Gineste <gnj4282a@u6-100-11d.univ-tlse3.fr>
2023-04-21 14:56:29 +0200

    Init projet

---
+++ test/ilu2/WelcomeTest.java
@@ -0,0 +1,22 @@
+package ilu2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class WelcomeTest {
+	
+	Welcome wel;
+	
+	@BeforeEach
+	void setup() {
+		wel = new Welcome();
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
